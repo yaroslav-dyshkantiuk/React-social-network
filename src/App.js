3d0765f437c631b94d12e0f33d0fa19a6c36c7e9
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2022-02-07 14:48:02 +0200

    UI BLl

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = (props) => {
-    return (
-        <BrowserRouter>
-            <div className='app-wrapper'>
-                <Header/>
-                <Navbar/>
-                <div class='app-wrapper-content'>
-                    <Route path='/dialogs' component={Dialogs}/> {/*/dialogs/spam/blabla*/}
-                    <Route path='/profile' component={Profile}/>
-                </div>
-            </div>
-        </BrowserRouter>)
-}
+  return (
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/dialogs/*' element={<Dialogs />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import s from './Post.module.css';
 
 const Post = (props) => {
-
   return (
     <div className={s.item}>
-      <img src='https://movies4maniacs.liberty.me/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg' />
-        { props.message }
-          <div>
-        <span>like</span> { props.likesCount }
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbnAjbVaBzAMY62VtS6ddKTMGeD6n3Qgrs0g&usqp=CAU' />
+      {props.message}
+      <div>
+        <span>like</span> {props.likesCount}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Post;
